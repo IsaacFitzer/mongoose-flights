2ac3f807da22fd16a0ab7fa092e4f101fa5f27c8
IsaacFitzer <fitzerisaac@gmail.com>
2023-02-21 21:14:16 -0500

    part 2 (working)

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -6,12 +6,8 @@ module.exports = {
 
 function create(req, res) {
   Flight.findById(req.params.id, function(err, flight) {
-    console.log("req.body", req.body)
     flight.destinations.push(req.body);
-    console.log("flight", flight)
-    console.log("req.body", req.body)
     flight.save(function(err) {
-        console.log("destinations", flight.destinations)
       res.redirect(`/flights/${flight._id}`);
     });
   });
